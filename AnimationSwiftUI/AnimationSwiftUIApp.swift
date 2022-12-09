4a30bde59440cbf5781b971a2a5cb8e6c2eb8d73
Sultan Rifaldy <sultanrifaldy@gmail.com>
2022-12-09 12:27:35 +0700

    Initial Commit

---
+++ AnimationSwiftUI/AnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationSwiftUIApp.swift
+//  AnimationSwiftUI
+//
+//  Created by Sultan Rifaldy on 09/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
